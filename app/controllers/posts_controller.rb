3c776ff1bb0c63bc220fe0f199ed250f6adce3a1
devondaphenom <dseth.johnson@gmail.com>
2017-08-07 05:39:37 +0000

    Indivudal pic fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,34 @@
-class PostsController < ApplicationController
-  
-  def index
-    @posts = Post.all
+  class PostsController < ApplicationController
+    
+    def index
+      @posts = Post.all
+    end
+    
+    
+   def post
+     @post = Post.first
+   end
+   
+   def new
+    @post = Post.new
   end
+    
+    def create
+      @post = Post.create(post_params)
+      redirect_to posts_path
+    end
+    
+    def show
+      @post = Post.find(params[:id])
+    
+    end
   
+    private
+    
+    def post_params
+      params.require(:post).permit(:image, :caption)
+    end
+    
   
- def post
-   @post = Post.first
- end
  
- def new
-  @post = Post.new
-end
-  
-  def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
   end
-  
- 
-
-  private
-  
-  def post_params
-    params.require(:post).permit(:image, :caption)
-  end
-end
