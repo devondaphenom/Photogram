1896a0144f5160c2352179976e53124c60807058
devondaphenom <dseth.johnson@gmail.com>
2017-08-07 06:17:49 +0000

    Edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,16 @@
       @post = Post.find(params[:id])
     
     end
+    
+    def edit
+      @post = Post.find(params[:id])
+    end
+    
+    def update
+      @post = Post.find(params[:id])
+      @post.update(post_params)
+      redirect_to(post_path(@post))
+    end
   
     private
     
